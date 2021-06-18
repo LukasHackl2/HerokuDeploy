77c4ad937f0c04f4ac7dacd55653a5ca4f546502
Lukas <lukas.h95hotmail.de>
2021-06-18 21:55:17 +0200

    sdflksdjf

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + 'frontend\dist\frontend'));
+app.use(express.static(__dirname + 'frontend/dist/frontend'));
 
 // CORS HEADERS MIDDLEWARE
 app.use(function (req, res, next) {
